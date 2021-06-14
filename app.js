31a69a4e67aaea1e56664943f03604a9dd11c91d
Leonardo Galeano <leo22578@gmail.com>
2021-06-14 15:26:32 -0300

    Agrego el procfile

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
 
 
-app.listen(3030, () => {
-    console.log("Servidor funcionando")
-});
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Servidor corriendoen el puerto 3000")
+})
 
 app.get ("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html")); 
